d8f7f37c5410da6952fc0e6d928af301ccae61dd
Aleksandr <a@bukhalo.com>
2019-02-25 14:16:44 +0300

    Release v0.2.0 (#33)

---
+++ src/lib/parseURL.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+async function shazamParse(url) {
+  if (!url) return false;
+  const id = url.split('/').filter(one => {
+    return !isNaN(one) && one !== '';
+  });
+  const template = `https://www.shazam.com/discovery/v4/ru/US/web/-/track/${id[0]}`;
+  const res = await axios(template);
+  if (!res.data) return '';
+  const result =
+    res.data.hub.options.apple.openin.actions[0].uri ||
+    res.data.hub.options.spotify.openin.actions[0].uri ||
+    '';
+  return result;
+}
+
+export default async text => {
+  const regexp = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g;
+  const answer = text.match(regexp);
+  if (answer && answer[0] && answer[0].match(/shazam/)) {
+    return shazamParse(answer[0]);
+  }
+  return answer ? answer[0] : '';
+};
