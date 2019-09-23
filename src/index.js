b5a93c7e6316a73e3c81f9528d4d209aa88c5e47
Aleksandr Bukhalo <a@bukhalo.com>
2019-09-23 16:35:14 +0300

    AWS Lambda support (#68)

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import middlewares from './middlewares';
 
 const bot = new Telegraf(process.env.TOKEN);
 bot.use(middlewares);
-bot.launch();
+
+// bot.launch();
+
+/* AWS Lambda handler function */
+export const handler = (event, context, callback) => {
+  const tmp = JSON.parse(event.body);
+  bot.handleUpdate(tmp);
+  return callback(null, {
+    statusCode: 200,
+    body: ''
+  });
+};
+
+export default handler;
