48e51e35aba19ddd195658ad307dfcfd9d345042
Aleksandr Bukhalo <a@bukhalo.com>
2020-02-29 15:47:35 +0300

    feat(configuration): write database module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SentryModule } from './sentry/sentry.module';
 import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 // import { SpotifyModule } from './spotify/spotify.module';
 import { LinksModule } from './links/links.module';
 import { OdeslyModule } from '@app/odesly';
@@ -9,6 +10,7 @@ import { OdeslyModule } from '@app/odesly';
   imports: [
     SentryModule,
     ConfigModule,
+    DatabaseModule,
     OdeslyModule,
     // SpotifyModule,
     LinksModule,
