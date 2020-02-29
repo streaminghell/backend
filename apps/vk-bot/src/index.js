1c8e7239876e70b85b6518bb66488b1b95fac6aa
Aleksandr Bukhalo <a@bukhalo.com>
2020-02-29 15:57:58 +0300

    refactor: remove api and vk-bot apps

--- apps/vk-bot/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import VkBot from 'node-vk-bot-api';
-import message from './lib/message';
-
-const bot = new VkBot(process.env.VK_BOT_TOKEN);
-
-bot.event('message_new', message);
-
-bot.startPolling();
