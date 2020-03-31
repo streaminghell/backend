d667d08233d60588bb97c9fdd16d1d2cb386fd68
Aleksandr Bukhalo <a@bukhalo.com>
2020-03-31 15:19:14 +0300

    fix: change routes global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api/v1-alpha.1');
+  app.setGlobalPrefix('v1');
 
   const options = new DocumentBuilder()
     .setTitle('Streaming Hell API')
     .setDescription('')
-    .setVersion('v1-alpha.1')
+    .setVersion('v1')
     .addTag('')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api/v1-alpha.1', app, document);
+  SwaggerModule.setup('v1', app, document);
 
   await app.listen(4000);
 }
